91c1499f8c507f904554cb105a533243f52a799b
Adam H. Leventhal <ahl@oxide.computer>
2024-11-14 18:13:35 -0800

    transfer over 11ty config

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+export default {
+	title: "Adam Leventhal's Blog",
+	url: "https://ahl.dtrace.org/",
+	language: "en",
+	description: "Adam Leventhal's Blog",
+	author: {
+		name: "Adam Leventhal",
+		email: "",
+		url: ""
+	}
+}
